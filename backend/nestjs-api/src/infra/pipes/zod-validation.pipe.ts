12cb581c450f9f31f836f99f2d656896c8d116c5
Jardel Bordignon <jardel1101@gmail.com>
2024-02-04 11:27:07 -0300

    zod validation pipe

---
+++ backend/nestjs-api/src/infra/pipes/zod-validation.pipe.ts
@@ -0,0 +1,81 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+  UsePipes,
+  applyDecorators,
+} from '@nestjs/common'
+import { ZodError, ZodObject, ZodSchema, ZodType, z } from 'zod'
+
+type ZodObj<T extends Record<PropertyKey, unknown>> = {
+  [key in keyof T]: ZodType<T[key]>
+}
+
+/**
+ * type ObjType = { name: string }
+ *
+ * const myZodObj = zodObject<ObjType>({ name: z.string() })
+ */
+function zodObject<T extends Record<PropertyKey, unknown>>(schema: ZodObj<T>) {
+  return schema as ZodObject<ZodObj<T>>
+}
+
+class ZodValidationError extends BadRequestException {
+  constructor(errors: ZodError['errors']) {
+    const details = errors.map(({ path, ...rest }) => ({
+      field: path.join('.'),
+      ...rest,
+    }))
+
+    super({
+      details,
+      error: 'InputData',
+      statusCode: 400,
+    })
+  }
+
+  static example(field = 'name') {
+    const errors: ZodError['errors'] = [
+      {
+        code: 'invalid_type',
+        expected: 'string',
+        message: 'Required',
+        path: [field],
+        received: 'undefined',
+      },
+    ]
+    return new this(errors).getResponse()
+  }
+}
+
+class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    if (!value) return
+
+    const isNotBodyParam = metadata.type !== 'body'
+    const isFile = typeof value === 'object' && 'fieldname' in value
+
+    if (isNotBodyParam || isFile) return value
+
+    try {
+      return this.schema.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new ZodValidationError(error.errors)
+      }
+
+      throw new BadRequestException('Validation failed')
+    }
+  }
+}
+
+/**
+ * @UseZodValidation(someZodObject)
+ */
+function UseZodValidation(schema: ZodSchema) {
+  return applyDecorators(UsePipes(new ZodValidationPipe(schema)))
+}
+
+export { UseZodValidation, zodObject, z, ZodValidationError }
